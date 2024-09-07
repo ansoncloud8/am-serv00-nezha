d697d944b0ba19bba9c2cbb58825cc0dd8a486e6
AM科技 <137372060+ansoncloud8@users.noreply.github.com>
2024-09-08 00:00:03 +0800

    Update install-agent.sh

--- install-agent.sh
+++ install-agent.sh
@@ -92,10 +92,14 @@ ZIP_FILE="${TMP_DIRECTORY}/nezha-agent_freebsd_amd64.zip"
 [ ! -e ${WORKDIR}/start.sh ] && generate_run_agent
 
 if [ ! -e "${WORKDIR}/nezha-agent" ] || [ -n "${VERSION}" ]; then
-    download_agent
+    download_agent \
+    && decompression "${ZIP_FILE}" \
+    && install_agent
 fi
 
 decompression "${ZIP_FILE}" \
 && install_agent
 rm -rf "${TMP_DIRECTORY}"
 [ -e ${WORKDIR}/start.sh ] && run_agent
+
+
